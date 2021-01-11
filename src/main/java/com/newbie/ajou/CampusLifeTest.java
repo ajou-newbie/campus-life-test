fd77de56807418e77f5731123b4eca097314fb1c
opdshe <opdshe@naver.com>
2021-01-11 16:34:09 +0900

    feat: 스켈레톤 코드 업로드

---
+++ src/main/java/com/newbie/ajou/CampusLifeTest.java
@@ -0,0 +1,12 @@
+package com.newbie.ajou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusLifeTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusLifeTest.class, args);
+	}
+}
