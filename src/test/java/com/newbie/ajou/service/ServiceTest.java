27ab6b2169ae4291d94079e03af9aac660839730
opdshe <opdshe@naver.com>
2021-01-25 19:07:22 +0900

    test: 설문 결과 저장 기능 동작 확인

--- src/test/java/com/newbie/ajou/service/ServiceTest.java
+++ src/test/java/com/newbie/ajou/service/ServiceTest.java
@@ -1,17 +1,43 @@
 package com.newbie.ajou.service;
 
+import com.newbie.ajou.domain.college.College;
+import com.newbie.ajou.domain.college.CollegeRepository;
+import com.newbie.ajou.domain.user.UserRepository;
 import com.newbie.ajou.web.dto.ResultRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 public class ServiceTest {
+	private static final String TEST_COLLEGE_NAME = "의학";
 
 	@Autowired
 	private ResultService resultService;
+
+	@Autowired
+	private CollegeRepository collegeRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeEach
+	void setUp() {
+		College college = new College(TEST_COLLEGE_NAME);
+		collegeRepository.save(college);
+	}
+
+	@AfterEach
+	void init() {
+		userRepository.deleteAll();
+		collegeRepository.deleteAll();
+	}
 
 	@Test
 	public void 결과_url_매핑_동작_확인() {
@@ -26,5 +52,20 @@ public class ServiceTest {
 
 		//then
 		assertThat(address).isEqualTo("/result/0101");
+	}
+
+	@Test
+	void 설문_결과_저장_기능_동작_확인() {
+		//given
+		int[] answers = new int[]{0, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2};
+		ResultRequestDto resultRequestDto = new ResultRequestDto(answers, TEST_COLLEGE_NAME);
+
+		//when
+		resultService.saveResult(resultRequestDto.getCollege(), "0101");
+		College collage = collegeRepository.findByName(TEST_COLLEGE_NAME);
+
+		//then
+		assertThat(collage.getIsfj()).isEqualTo(1);
+		assertThat(userRepository.findAll().size()).isEqualTo(1);
 	}
 }
--- src/test/java/com/newbie/ajou/web/controller/CampusLifeTestControllerTest.java
+++ src/test/java/com/newbie/ajou/web/controller/CampusLifeTestControllerTest.java
@@ -1,7 +1,11 @@
 package com.newbie.ajou.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.newbie.ajou.domain.college.College;
+import com.newbie.ajou.domain.college.CollegeRepository;
+import com.newbie.ajou.domain.user.UserRepository;
 import com.newbie.ajou.web.dto.ResultRequestDto;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -20,8 +24,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class CampusLifeTestControllerTest {
+	private static final String TEST_COLLEGE_NAME = "의학";
+
 	@Autowired
 	WebApplicationContext context;
+
+	@Autowired
+	private CollegeRepository collegeRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	private MockMvc mvc;
 
@@ -30,14 +42,21 @@ class CampusLifeTestControllerTest {
 		mvc = MockMvcBuilders
 				.webAppContextSetup(context)
 				.build();
+		College college = new College(TEST_COLLEGE_NAME);
+		collegeRepository.save(college);
+	}
+
+	@AfterAll
+	void clearUp() {
+		userRepository.deleteAll();
+		collegeRepository.deleteAll();
 	}
 
 	@Test
 	void ResultRequestDto_매핑_동작_확인() throws Exception {
 		//given
-		int[] answers = new int[]{0, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3};
-		String college = "의학";
-		ResultRequestDto dto = new ResultRequestDto(answers, college);
+		int[] answers = new int[]{0, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2};
+		ResultRequestDto dto = new ResultRequestDto(answers, TEST_COLLEGE_NAME);
 
 		//when
 		MvcResult result = mvc.perform(post("/result")
@@ -49,6 +68,6 @@ class CampusLifeTestControllerTest {
 		String resultUrl = result.getResponse().getContentAsString();
 
 		//then
-		assertThat(resultUrl).isEqualTo("/result/1111");
+		assertThat(resultUrl).isEqualTo("/result/0101");
 	}
 }
