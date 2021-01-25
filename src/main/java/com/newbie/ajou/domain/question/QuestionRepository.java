c072a7ab7167ac1f9f3404dbb623d0211d3fef9a
opdshe <opdshe@naver.com>
2021-01-25 19:09:57 +0900

    refactor: Question 데이터 받아올 때 Question에 속한 Choices 정보도 함께 받아오도록 수정

--- src/main/java/com/newbie/ajou/domain/question/QuestionRepository.java
+++ src/main/java/com/newbie/ajou/domain/question/QuestionRepository.java
@@ -1,6 +1,12 @@
 package com.newbie.ajou.domain.question;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+	@EntityGraph(attributePaths = {"choices"})
+	@Override
+	List<Question> findAll();
 }
