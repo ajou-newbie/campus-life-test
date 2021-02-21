868bea464f60ee57c5272ff2a641a59c163eafc5
Beaneee <whddkrak@gmail.com>
2021-02-21 14:43:24 +0900

    feat: 유형별 result pages 구현

---
+++ src/main/webapp/src/components/Questions/ProgressBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProgressBar = (props) => {
+  const { bgcolor, completed } = props;
+
+  const ContainerStyles = styled.div`
+    height: 9px;
+    width: 100%;
+    background-color: white;
+    border-radius: 20px;
+    margin: 0;
+`;
+
+  const FillerStyles = styled.div`
+    height: 100%;
+    width: ${completed/10*100}%;
+    background-color: #001981;
+    border-radius: inherit;
+    text-align: right;
+    transition: 0.5s;
+  `;
+
+  return (
+      <ContainerStyles>
+        <FillerStyles/>
+      </ContainerStyles>
+  );
+};
+
+export default ProgressBar;
